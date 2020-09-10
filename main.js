a62d4ea78eff7e47564b13f53187381f6b0f987f
LazyMudro <69349309+LazyMudro@users.noreply.github.com>
2020-09-10 19:15:25 +0500

    Add files via upload
    Добавлена функция Hightscore, которая берется из LocalStorage

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
-    car = document.createElement('div');
+    car = document.createElement('div'),
+    hightscore = document.querySelector('.hightscore');
+
 car.classList.add('car');
 
 start.addEventListener('click', startGame);
@@ -18,9 +20,22 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
+    hightscoreValue: 0,
     speed: 5,
-    traffic: 3
+    traffic: 2
 };
+
+if (localStorage.prevHightscore) {
+    setting.hightscoreValue = Number(localStorage.prevHightscore);
+} else {
+    localStorage.setItem('prevHightscore', 0);
+}
+
+function setHitghtscore() {
+    if (setting.score > setting.hightscoreValue) {
+        setting.hightscoreValue = setting.score;
+    }
+}
 
 function getQuantityElemets(heightElement) {
     return document.documentElement.clientHeight / heightElement + 1;
@@ -28,9 +43,10 @@ function getQuantityElemets(heightElement) {
 
 
 
+
 function startGame() {
     start.classList.add('hide');
-
+    gameArea.innerHTML = '';
     for (let i = 0; i < getQuantityElemets(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -47,25 +63,35 @@ function startGame() {
         enemy.style.background = 'transparent url(./image/enemy2.png) center / cover no-repeat';
         gameArea.appendChild(enemy);
     }
-
+    setting.score = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
-    if (setting.start === true) {
+    if (setting.start) {
+        setting.score += setting.speed;
+        setHitghtscore()
+        score.innerHTML = 'SCORE<br> ' + setting.score;
         moveRoad();
+        hightscore.innerHTML = 'HIGHTSCORE: ' + setting.hightscoreValue;
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
             setting.x -= setting.speed;
-        } else if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - 50)) {
+        }
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - 50)) {
             setting.x += setting.speed;
-        } else if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - 102)) {
+        }
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - 102)) {
             setting.y += setting.speed;
-        } else if (keys.ArrowUp && setting.y > 0) {
+        }
+        if (keys.ArrowUp && setting.y > 0) {
             setting.y -= setting.speed;
         }
 
@@ -101,6 +127,22 @@ function moveRoad() {
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
     enemy.forEach(function (item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom - 2 &&
+            carRect.right >= enemyRect.left + 2 &&
+            carRect.left <= enemyRect.right - 2 &&
+            carRect.bottom >= enemyRect.top + 2) {
+            if (Number(localStorage.getItem('prevHightscore')) < setting.hightscoreValue) {
+                localStorage.setItem('prevHightscore', Number(setting.hightscoreValue))
+                alert('Вы побили рекорд!')
+            }
+            setting.start = false;
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight;
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y >= document.documentElement.clientHeight) {
